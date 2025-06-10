ed2574c55ebe5f94c1ae1cef8fa6b77e407b8426
Krisna <your-email@example.com>
2025-06-10 19:21:11 +0700

    blok ini akan dieksekusi

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -7,4 +7,8 @@ int main () {
         throw 0.5; // melemparkan sebuah integer maka
         cout << "Pernytssn tidak akan dieksekusi" << endl;
     }
+    catch (int a) {
+        // blok ini akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
 }
