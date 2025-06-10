8af509d922d5ee1a0c6ba445b7d7ecded6e9daa7
Krisna <your-email@example.com>
2025-06-10 19:16:10 +0700

    int main

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -8,20 +8,4 @@ using namespace std;
 int main()
 {
     cout << "Awal program" << endl; //penanda 1:...
-    try{
-        array<int, 3> data= {10, 20, 30 };
-        //pesan array integer 3 elemen
-        cout << data. at (5) << endl;
-        //memanggil array elemen ke 5
-    }
-    catch (exception& e) {
-        //penangkap menggunakan objek exeption
-        cout << e. what() << endl;
-        /*akan dieksekusi karena array data hanya memiliki 3 elemen*/
-    }
-    cout << "Baris Program yang terakhir" << endl;
-    /*Petanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
-    return 0;
-    
-
 }
